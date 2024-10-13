003a39e6bfc93c561e0f5d8a2021fce78a8d82bb
apatil3142 <patilaniket2236@gmail.com>
2024-10-14 00:30:50 +0530

    change getBooks function placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import styles from "./page.module.css";
 import { IBook } from "@/lib/interface";
 export const dynamic = 'force-dynamic'
 
-const getBooks = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {cache: 'no-store'});
-  const data = await response.json();
-  return data;
-};
-
 export default async function Home() {
+  const getBooks = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {cache: 'no-store'});
+    const data = await response.json();
+    return data;
+  };
+
   const booksList: IBook[] = await getBooks();
 
   return (
