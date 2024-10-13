6ff4a74be8313b971d20e84ba26124d2ff75252e
apatil3142 <patilaniket2236@gmail.com>
2024-10-13 23:55:11 +0530

    force-dynamic in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Bookcard from "@/components/bookcard/Bookcard";
 import styles from "./page.module.css";
 import { IBook } from "@/lib/interface";
+export const dynamic = 'force-dynamic'
 
 const getBooks = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {cache: 'no-store'});
