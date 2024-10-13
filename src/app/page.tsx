d2a1f09f8f4a1271cb6f6d83b73f96ba24a84aad
apatil3142 <patilaniket2236@gmail.com>
2024-10-13 23:47:24 +0530

    no-cache ==> no-store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import styles from "./page.module.css";
 import { IBook } from "@/lib/interface";
 
 const getBooks = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {cache: 'no-cache'});
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/books`, {cache: 'no-store'});
   const data = await response.json();
   return data;
 };
 
 export default async function Home() {
   const booksList: IBook[] = await getBooks();
-  
+
   return (
     <div className={styles.container}>
       {booksList?.map(book => (
